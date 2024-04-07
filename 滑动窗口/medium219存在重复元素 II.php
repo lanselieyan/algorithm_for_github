54d3d0ff001e05e119d962887a9277e3c536a88e
lanselieyan <2224314592@qq.com>
2024-04-07 22:59:36 +0800

    p

--- 滑动窗口/medium219存在重复元素 II.php
+++ 滑动窗口/medium219存在重复元素 II.php
@@ -18,6 +18,11 @@ class Solution {
                 // 当窗口的大小大于 k 时，缩小窗口
                 $index = array_search($nums[$left], $window);
                 if ($index !== false) {
+                    //array_splice() 函数是 PHP 中用于对数组进行删除、插入和替换操作的函数之一。
+                    //在这种情况下，array_splice($window, $index, 1) 表示从数组 $window 中删除指定索引位置 $index 处的元素，
+                    //并且只删除一个元素（第三个参数为1）。
+                    //删除后，数组中的其他元素会向前移动以填补空缺。该函数的返回值是被删除的元素（以数组形式返回）。
+                    //简而言之，array_splice($window, $index, 1) 的作用是从数组中删除指定索引位置的一个元素。
                     array_splice($window, $index, 1);
                 }
                 $left++;
