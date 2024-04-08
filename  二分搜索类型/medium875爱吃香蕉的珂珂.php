262b51ef77f7b0e8ba98a9fe92a655c05ea9ee44
lanselieyan <2224314592@qq.com>
2024-04-08 09:24:59 +0800

    p

---  二分搜索类型/medium875爱吃香蕉的珂珂.php
+++  二分搜索类型/medium875爱吃香蕉的珂珂.php
@@ -1,1 +1,28 @@
 <?php
+function minEatingSpeed($piles, $H) {
+    $left = 1;
+    $right = 1000000000 + 1;
+
+    while ($left < $right) {
+        $mid = $left + intval(($right - $left) / 2);
+        if (f($piles, $mid) == $H) {
+            // 搜索左侧边界，则需要收缩右侧边界
+            $right = $mid;
+        } else if (f($piles, $mid) < $H) {
+            // 需要让 f(x) 的返回值大一些
+            $right = $mid;
+        } else if (f($piles, $mid) > $H) {
+            // 需要让 f(x) 的返回值小一些
+            $left = $mid + 1;
+        }
+    }
+    return $left;
+}
+
+function f($piles, $K) {
+    $hours = 0;
+    foreach ($piles as $pile) {
+        $hours += ceil($pile / $K);
+    }
+    return $hours;
+}
