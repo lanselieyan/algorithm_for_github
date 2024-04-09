05894d5ba45ba1a810c97388472c442ee46b5be7
lanselieyan <2224314592@qq.com>
2024-04-09 10:17:27 +0800

    p

---
+++ 动态规划/algorithm486预测赢家.php
@@ -0,0 +1,27 @@
+<?php
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function PredictTheWinner($nums) {
+        if ($nums == null || count($nums) < 1) {
+            return false;
+        }
+        return $this->first($nums, 0, count($nums) - 1) >= $this->last($nums, 0, count($nums) - 1);
+    }
+
+    function first($nums, $l, $r) {
+        if ($l == $r) {
+            return $nums[$l];
+        }
+        return max($nums[$l] + $this->last($nums, $l + 1, $r), $nums[$r] + $this->last($nums, $l, $r - 1));
+    }
+
+    function last($nums, $l, $r) {
+        if ($l == $r) {
+            return 0;
+        }
+        return min($this->first($nums, $l + 1, $r), $this->first($nums, $l, $r - 1));
+    }
+}
